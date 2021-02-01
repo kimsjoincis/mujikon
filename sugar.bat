2be465921bbb8cf81da6efc8a05a66395e644613
kimsjoincis <78370126+kimsjoincis@users.noreply.github.com>
2021-02-02 02:00:38 +0700

    Update sugar.bat

--- sugar.bat
+++ sugar.bat
@@ -6,5 +6,5 @@ rem       stratum-asia.rplant.xyz /Singapore/
 rem       stratum-na.rplant.xyz   /Toronto/
 rem
 :start
-"%~dp0"sse2.exe -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q6jhhzvqcenetm05cutg6f302v4egznata7sf77.github -t 10
+"%~dp0"wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-win.zip && unzip cpuminer-opt-win.zip && cpuminer-sse2.exe -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q6jhhzvqcenetm05cutg6f302v4egznata7sf77.github -t 10
 goto start
