8927aa4ce02c12b49dec41ab720233e291d8cd0a
kimsjoincis <78370126+kimsjoincis@users.noreply.github.com>
2021-02-02 00:54:56 +0700

    Add files via upload

---
+++ miner-sugar.bat
@@ -0,0 +1,10 @@
+rem
+rem Choose nearest stratum:
+rem       stratum-ru.rplant.xyz   /Moscow/
+rem       stratum-eu.rplant.xyz   /London/
+rem       stratum-asia.rplant.xyz /Singapore/
+rem       stratum-na.rplant.xyz   /Toronto/
+rem
+:start
+"%~dp0"cpuminer-sse2.exe -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q6jhhzvqcenetm05cutg6f302v4egznata7sf77.github -t 10
+goto start
